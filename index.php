e41b8d129188f114b696f56121e8b08180ca3d61
tofaelalam <90176624+tofaelalam@users.noreply.github.com>
2021-09-05 23:58:20 -0700

    Add test purposes

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alumni Management System</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="page1.css">
+	<style>
+		.navback{
+			background-color:#3222c3;
+		}
+	</style>
+</head>
+<body>
+		<div class="banner">
+		
+		</div><!--
+		<div class="middle">
+		 <marquee><b>Every Alumni are asset for Us</b></marquee>
+		</div>  -->
+		
+		<nav class="navbar navbar-inverse navback">
+  <div class="container-fluid">
+  <!--
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>-->
+    <ul class="nav navbar-nav">
+      <li ><a href="#">Home</a></li>
+      <li><a href="alumni_list.php">Alumni</a></li>
+	  <li><a href="gallery.php">Gallery</a></li>
+	  <li><a href="#">About</a></li>
+		<li><a href="#">Policy</a></li>
+		
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="Pform.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="loginform.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+		
+		
+	<div class="body">
+		<div class="welcome">
+			<h2>Welcome To Alumni Management System</h2>
+		</div>
+		
+		
+			
+			
+		<!--
+		<div class="button1" >
+			<a href="">
+				<button class="btn1">Alumni</button>
+			</a>
+			<a href=".php">
+				<button class="btn1">Terms/Policy</button>
+			</a>
+			<a href=".php">
+				<button class="btn1">About-Us</button>
+			</a>
+			<a href="">
+				<button class="btn1">Gallery</button>
+			</a>
+			<a href="">
+				<button class="btn1">Log-In</button>
+			</a>
+			<a href="">
+				<button class="btn1">Create Account</button>
+			</a>
+			
+		</div>-->
+	</div>	
+</body>
+</html>
